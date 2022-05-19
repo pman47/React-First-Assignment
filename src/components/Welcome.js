8e51f5ad95c083485a10769793502a3566f8859c
Manish <manishprajapatim416@gmail.com>
2022-05-19 18:58:19 +0530

    .

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,8 +2,8 @@ import React from "react";
 const Welcome = (props) => {
   return (
     <>
-      <h1>Hey ! {props.name}</h1>
-      <h2>Welcome to Newton School</h2>
+      <h1>Hey !{props.name}</h1>
+      <h2>Welcome to Newton Schoo.l</h2>
     </>
   );
 };
